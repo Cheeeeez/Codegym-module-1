8ae075024d35da925688a55fa21f0fa25782b347
Cheeeeez <nguyentrongnama8@gmail.com>
2020-03-15 02:34:27 +0700

    change variables name

--- Array/Bai-Tap/tu-dien/myjs.js
+++ Array/Bai-Tap/tu-dien/myjs.js
@@ -3,10 +3,10 @@ function translateEtoV() {
     let english = ['apple', 'orange', 'banana', 'mango'];
     let vietnamese = ['quả táo', 'quả cam', 'quả chuối', 'quả xoài'];
     let input = document.getElementById('input_box').value;
-    let index = english.indexOf(input);
-    if (index === -1) {
+    let check = english.indexOf(input);
+    if (check === -1) {
         document.getElementById('result').innerHTML = "Không tìm thấy !";
     } else {
-        document.getElementById('result').innerHTML = vietnamese[index];
+        document.getElementById('result').innerHTML = vietnamese[check];
     }
 }
